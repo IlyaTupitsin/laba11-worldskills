20564c28e3aca8df4c11773c5a08a957c4c2a214
Илья Тупицин <ilhatupitsin@yandex.ru>
2024-05-11 11:45:12 +0300

    Добавьте файлы проекта.

---
+++ лаба11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    delegate string DayOfWeekDelegate();
+
+    public static void Main()
+    {
+        string[] daysOfWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+        int DayIndex = 0;
+
+        DayOfWeekDelegate getDayOfWeek = () =>
+        {
+            string day = daysOfWeek[DayIndex];
+            DayIndex = (DayIndex + 1) % daysOfWeek.Length;
+            return day;
+        };
+
+        for (int i = 0; i < 8; i++)
+        {
+            Console.WriteLine(getDayOfWeek());
+        }
+    }
+}
